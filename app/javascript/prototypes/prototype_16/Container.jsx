adb5da4078bfac403995be316aee2584fa7a0397
winteresy <alisagurova2@gmail.com>
2023-03-28 14:08:26 +0300

    aurora new

--- app/javascript/prototypes/prototype_16/Container.jsx
+++ app/javascript/prototypes/prototype_16/Container.jsx
@@ -6,7 +6,15 @@ import p5 from "p5";
 import image1 from "../../../assets/images/image1.jpg";
 import image2 from "../../../assets/images/image2.jpg";
 import image3 from "../../../assets/images/image3.jpg";
-const images = [image1, image2, image3];
+import image4 from "../../../assets/images/image4.jpg";
+import image5 from "../../../assets/images/image5.jpg";
+import image6 from "../../../assets/images/image6.jpg";
+import image7 from "../../../assets/images/image7.jpg";
+import image8 from "../../../assets/images/image8.jpg";
+import image9 from "../../../assets/images/image9.jpg";
+import image10 from "../../../assets/images/image10.jpg";
+import image11 from "../../../assets/images/image11.jpg";
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11];
 
 
 
@@ -34,7 +42,7 @@ function Prototype16() {
     const sketch = (p) => {
       p.setup = () => {
         canvasRef.current = p.createCanvas(700, 400);
-        //canvasRef.current.style("filter", "blur(40px)"); 
+        canvasRef.current.style("filter", "blur(40px)"); 
         p.colorMode(p.HSB, 100, 100, 100, 255);
         p.frameRate(1);
       };
@@ -52,7 +60,7 @@ function Prototype16() {
           p.noStroke();
           p.ellipse(x, y, p.random(20, 50), p.random(60, 150));
         }
-        p.drawingContext.filter = 'blur(40px)';
+        //p.drawingContext.filter = 'blur(40px)';
 
         
       };
@@ -91,48 +99,18 @@ function Prototype16() {
     }
   }
 
+  const start = document.querySelector('#start');
   const toggleContent = () => {
     if (!showContent) {
       startCanvas();
+      start.classList.add('hidden');
     } else {
       stopCanvas();
     }
     setShowContent(!showContent);
   };
 
-  // const handleSaveImage = () => {
-  //   const backgroundImageElement = document.querySelector(".background");
-  //   const canvas = canvasRef.current.canvas;
-  //   const tempCanvas = document.createElement("canvas");
-  //   const tempContext = tempCanvas.getContext("2d");
-  //   const tempImage = new Image();
   
-  //   const backgroundImage = new Image();
-  //   backgroundImage.src = backgroundImageElement.style.backgroundImage.slice(4, -1).replace(/"/g, "");
-  //   backgroundImage.onload = () => {
-  //     tempCanvas.width = backgroundImage.width;
-  //     tempCanvas.height = backgroundImage.height;
-  //     tempContext.drawImage(backgroundImage, 0, 0);
-  
-  //     tempContext.drawImage(canvas, 0, 0);
-  
-  //     tempContext.filter = "blur(40px)";
-  //     tempContext.drawImage(tempCanvas, 0, 0);
-
-      
-  //     const canvasImage = tempCanvas.toDataURL("image/jpg");
-  //     setCanvasImage(canvasImage);
-  
-  //     const downloadLink = document.createElement("a");
-  //     downloadLink.download = "canvas.jpg";
-  //     downloadLink.href = canvasImage;
-  //     downloadLink.click();
-  //   };
-  
-  //   tempImage.src = backgroundImageElement.style.backgroundImage.slice(4, -1).replace(/"/g, "");
-  // };
-    
-
 
   const handleWaveAmplitudeChange = (event) => {
     setWaveAmplitude(event.target.value);
@@ -152,81 +130,97 @@ function Prototype16() {
     setWaveBrightness(event.target.value);
     setWaveColor([waveHue, waveSaturation, event.target.value]);
   };
-  var button = document.getElementById("start");
-  console.log(button);
-  //button.addEventListener("click", startCanvas);
 
   return (
     <div>
-
-<button id="start" onClick={() => {
-toggleContent();
-if (!canvasRunning) startCanvas();
-}}>{showContent ? "Hide" : "Show"}</button>
-      {showContent && (
-        <div>
-          <div className="background" style={{ backgroundImage }}>
-        <div ref={containerRef}></div>
-      </div>
-      <div className="input_container">
-      <div>
-        <label htmlFor="waveAmplitudeSlider">Wave Amplitude:</label>
-        <input
-          className="input"
-          type="range"
-          min="0"
-          max="100"
-          value={waveAmplitude}
-          onChange={handleWaveAmplitudeChange}
-          id="waveAmplitudeSlider"
-        />
-      </div>
-      <div>
-        <label htmlFor="waveHueSlider">Wave Hue:</label>
-        <input
-          className="input"
-          type="range"
-          min="0"
-          max="100"
-          value={waveHue}
-          onChange={handleWaveHueChange}
-          id="waveHueSlider"
-        />
-      </div>
-      <div>
-        <label htmlFor="waveSaturationSlider">Wave Saturation:</label>
-        <input
-          className="input"
-          type="range"
-          min="0"
-          max="100"
-          value={waveSaturation}
-          onChange={handleWaveSaturationChange}
-          id="waveSaturationSlider"
-        />
-      </div>
-      <div>
-        <label htmlFor="waveBrightnessSlider">Wave Brightness:</label>
-        <input
-          className="input"
-          type="range"
-          min="0"
-          max="100"
-          value={waveBrightness}
-          onChange={handleWaveBrightnessChange}
-          id="waveBrightnessSlider"
-        />
-      </div>
-      </div>
-      <div className="button_container">
-         {/* <button onClick={handleSaveImage}>Save Image</button> */}
-         <button onClick={() => handleImageChange()}>Change Background</button>
-      </div>
+      <div id="start" className="start" onClick={() => {
+        toggleContent();
+        if (!canvasRunning) startCanvas();
+        }}>
+        <div className="light_vector"></div>
+          <div className="light_start">
+          <div className="round_2"></div>
+          <div className="round_3"></div>
+          <button className="button_start" >{showContent ? "Hide" : "aurora"}</button>   
         </div>
-      )}
+        <p className="text_start">
+            the very first time i looked up to the sky,<br />
+            it was dark, and it was scary— still, <br />
+            i tried.the world was so cruel, <br />
+            and my mind was in chaos but someone told me, <br />
+            “it’ll be the most brilliant darkness in our eyes”
+        </p>
+        <div className="round_1"></div>
+        <div className="round_4"></div>
+    </div>
+          {showContent && (
+            <div>
+              <div className="background" style={{ backgroundImage }}>
+            <div ref={containerRef}></div>
+          </div>
+          <div className="input_container">
+          <div className="input_text_with_slider">
+            <label htmlFor="waveAmplitudeSlider">Wave Amplitude:</label>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="100"
+              value={waveAmplitude}
+              onChange={handleWaveAmplitudeChange}
+              id="waveAmplitudeSlider"
+            />
+          </div>
+          <div className="input_text_with_slider">
+            <label htmlFor="waveHueSlider">Wave Hue:</label>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="100"
+              value={waveHue}
+              onChange={handleWaveHueChange}
+              id="waveHueSlider"
+            />
+          </div>
+          <div className="input_text_with_slider">
+            <label htmlFor="waveSaturationSlider">Wave Saturation:</label>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="100"
+              value={waveSaturation}
+              onChange={handleWaveSaturationChange}
+              id="waveSaturationSlider"
+            />
+          </div>
+          <div className="input_text_with_slider">
+            <label htmlFor="waveBrightnessSlider">Wave Brightness:</label>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="100"
+              value={waveBrightness}
+              onChange={handleWaveBrightnessChange}
+              id="waveBrightnessSlider"
+            />
+          </div>
+          </div>
+          <div className="button_container">
+            <button className="button_change" onClick={() => handleImageChange()}>Change Background</button>
+          </div>
+            </div>
+          )}
 
-      
-      </div>
-  );
- }
+          
+          </div>
+      );
+    }
  export default Prototype16;
+
+
+
+
+ 
